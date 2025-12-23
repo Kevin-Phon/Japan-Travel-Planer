061a8d7ae25059a099beaaeeaf7dbf18e9e5c4f7
Kevin-Phon <kayaswp@gmail.com>
2025-12-22 21:04:31 -0800

    fix: Resolve race condition in useLocalStorage hook

--- hooks/useLocalStorage.ts
+++ hooks/useLocalStorage.ts
@@ -20,14 +20,16 @@ export function useLocalStorage<T>(key: string, initialValue: T) {
     const setValue = (value: T | ((val: T) => T)) => {
         try {
             // Allow value to be a function so we have same API as useState
-            const valueToStore =
-                value instanceof Function ? value(storedValue) : value;
-            // Save state
-            setStoredValue(valueToStore);
-            // Save to local storage
-            if (typeof window !== 'undefined') {
-                window.localStorage.setItem(key, JSON.stringify(valueToStore));
-            }
+            setStoredValue((prevValue) => {
+                const valueToStore = value instanceof Function ? value(prevValue) : value;
+
+                // Save to local storage
+                if (typeof window !== 'undefined') {
+                    window.localStorage.setItem(key, JSON.stringify(valueToStore));
+                }
+
+                return valueToStore;
+            });
         } catch (error) {
             console.log(error);
         }
